c535d45969b470b5ceef6e3324936d4d2b0585f9
Moussa-ehm <121896713+Moussa-ehm@users.noreply.github.com>
2023-01-03 21:58:00 +0100

    Delete Vue directory

--- Vue/erreur.php
+++
@@ -1,1 +0,0 @@
-<p>Une erreur est survenue : <?= $this->nettoyer($msgErreur) ?></p>
--- Vue/viewSuccessInscription.php
+++
@@ -1,6 +0,0 @@
-<?php ob_start(); ?>
-
-<p>Inscription réussie</p>
-<?php $contenu = ob_get_clean(); ?>
-
-<?php require_once 'viewGabarit.php'; ?>
